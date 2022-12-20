667e7fab77525193e2088f85013ae62e1e80b92a
prashant707 <prashantgahlot707@gmail.com>
2022-12-20 21:34:27 +0530

    feat : input to output text

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 //storing elements as object in variable
 var translateButton = document.querySelector("#input-button");
 var inputText = document.querySelector("#input-textarea");
+var outputText = document.querySelector("#output-text");
 
 //adding onclick eventlistener
 
 translateButton.addEventListener('click',translateToBanana);
 
 
-
+//
 
 function translateToBanana(){
     console.log("hello clicked transalate button",translateButton);
-    fetch()
+    outputText.innerHTML = inputText.value;
+
 }
