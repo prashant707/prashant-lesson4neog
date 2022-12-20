7c95b78eea558c0120219cd412ae0aea7b820329
prashant707 <prashantgahlot707@gmail.com>
2022-12-20 21:28:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+//storing elements as object in variable
+var translateButton = document.querySelector("#input-button");
+var inputText = document.querySelector("#input-textarea");
+
+//adding onclick eventlistener
+
+translateButton.addEventListener('click',translateToBanana);
+
+
+
+
+function translateToBanana(){
+    console.log("hello clicked transalate button",translateButton);
+    fetch()
+}
